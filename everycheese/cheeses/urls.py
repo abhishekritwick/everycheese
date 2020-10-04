0f4177e7c2776fd8567d166041c5e40f759ca637
Ritwick <abhishek.ritwick@gmail.com>
2020-10-05 04:23:58 +0530

    Add cheese update view, modify template

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -19,4 +19,9 @@ urlpatterns = [
         view=views.CheeseCreateView.as_view(),
         name="add"
     ),
+    path(
+        route='<slug:slug>/update/',
+        view=views.CheeseUpdateView.as_view(),
+        name='update'
+    ),
 ]
